947e0589d89892914e6a7239f9bf53166012b786
Alejandro Lozano <alex93lzn@gmail.com>
2020-04-29 13:32:19 +0200

    añado atributo idcategoria PK

--- src/main/java/com/alexlzn/model/Categoria.java
+++ src/main/java/com/alexlzn/model/Categoria.java
@@ -1,38 +1,51 @@
 package com.alexlzn.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Categoria {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idcategoria;
 	private String nombre;
 	private String descripcion;
-	
+
 	public Categoria() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Categoria(String nombre, String descripcion) {
-		super();
-		this.nombre = nombre;
-		this.descripcion = descripcion;
+
+	public int getIdcategoria() {
+		return idcategoria;
 	}
+
+	public void setIdcategoria(int idcategoria) {
+		this.idcategoria = idcategoria;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	@Override
 	public String toString() {
-		return "Categoria [nombre=" + nombre + ", descripcion=" + descripcion + "]";
+		return "Categoria [idcategoria=" + idcategoria + ", nombre=" + nombre + ", descripcion=" + descripcion + "]";
 	}
 	
+
 }
