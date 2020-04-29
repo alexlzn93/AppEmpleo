a8be4f1bb93d4ef35eec1a515c0d6c026891742e
Alejandro Lozano <alex93lzn@gmail.com>
2020-04-29 13:33:17 +0200

    añado metodos de eliminar y editar categorias

--- src/main/java/com/alexlzn/controller/CategoriasController.java
+++ src/main/java/com/alexlzn/controller/CategoriasController.java
@@ -3,10 +3,13 @@ package com.alexlzn.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.alexlzn.interfaces.ICategoriaService;
 import com.alexlzn.model.Categoria;
@@ -19,18 +22,38 @@ public class CategoriasController {
 	
 	@RequestMapping("/index") 
 	public String index(Model model) {
-		
+		categoriaService.findAllCategoria();
+		model.addAttribute("list_categorias", categoriaService.findAllCategoria());
 		return "categorias/listCategorias";
 	}
 	@RequestMapping("/create")
-	public String createCategoria(Model model,@ModelAttribute Categoria categoria) {
+	public String createCategoria(Model model,Categoria categoria) {
 		//FORMULARIO NUEVA CATEGORIA
 		return "categorias/formCategorias";
 	}
 	@PostMapping("/save")
-	public String saveCategoria(@ModelAttribute Categoria categoria, Model model) {
+	public String saveCategoria( Categoria categoria,BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			System.out.println("Ocurrio un error al introducir los datos del formulario");
+			return "vacantes/formVacante"; //si hay errores en el formulario devuelvo la vista 
+		}
+		attributes.addFlashAttribute("mensaje", "Categoria guardada correctamente"); //atributo flash
 		categoriaService.guardar(categoria);
-		//System.out.println(nombre + " " +descripcion);
-		return "categorias/listCategorias";
+	
+		return "redirect:/categorias/index";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("id") int idcategoria,Model model) {
+		model.addAttribute("id", idcategoria);
+		categoriaService.delete(idcategoria);
+		return "mensajeEliminado";
+	}
+	
+	@GetMapping("/editar")
+	public String editar(@RequestParam("id") int idcategoria,Model model) {
+		model.addAttribute("id", idcategoria);
+		categoriaService.buscarPorId(idcategoria);
+		return "redirect:/categorias/create";
 	}
 }
