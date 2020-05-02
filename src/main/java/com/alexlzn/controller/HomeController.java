280d15c185042c87cb09d66c33672256efee74ae
Alejandro Lozano <alex93lzn@gmail.com>
2020-05-02 16:01:10 +0200

    despliego option de listCategorias

--- src/main/java/com/alexlzn/controller/HomeController.java
+++ src/main/java/com/alexlzn/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.alexlzn.interfaces.ICategoriaService;
 import com.alexlzn.interfaces.IVacantesService;
 import com.alexlzn.model.Vacante;
 
@@ -16,12 +17,15 @@ import com.alexlzn.model.Vacante;
 public class HomeController {
 	@Autowired
 	IVacantesService vacanteService;
-	
+	@Autowired
+	ICategoriaService categoriasService;
 	@GetMapping("/")
-	public String goHome(Model model) {
+	public String goHome(Model model,Vacante vacante) {
 		//PAGINA PRINCIPAL
 		List<Vacante> listVacantes= vacanteService.findAllVacantes();
+		model.addAttribute("categorias", categoriasService.findAllCategoria());//lista de categorias en el index
 		model.addAttribute("vacantes", listVacantes);
+		model.addAttribute("mensaje", "Seleccione una categoría");
 		return "home/index"; //home es la carpeta donde estaran html del controlador HomeController
 	}
 
