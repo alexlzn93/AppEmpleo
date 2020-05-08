08b2e996e5ccfff0d04aaeeda677268b30fc9c14
Alejandro Lozano <alex93lzn@gmail.com>
2020-05-08 11:55:05 +0200

    CrudRepository IPerfilRepository

---
+++ src/main/java/com/alexlzn/repository/IPerfilRepository.java
@@ -0,0 +1,9 @@
+package com.alexlzn.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.alexlzn.model.Perfil;
+
+public interface IPerfilRepository extends CrudRepository<Perfil, Integer> {
+
+}
