eb9444a5aa4c108574f2ae5d269365b0fe57eeee
Alejandro Lozano <alex93lzn@gmail.com>
2020-05-18 18:37:36 +0200

    Hago un DAO de JpaRepository para que tenga paginacion

--- src/main/java/com/alexlzn/repository/IVacanteRepositoryJPA.java
+++ src/main/java/com/alexlzn/repository/IVacanteRepositoryJPA.java
@@ -1,9 +1,10 @@
 package com.alexlzn.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alexlzn.model.Vacante;
-
+@Repository
 public interface IVacanteRepositoryJPA extends JpaRepository<Vacante, Integer> {
 
 }
