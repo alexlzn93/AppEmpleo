4337e6f9e4cc09317d31b30c2e9a9a4fbb8b58e0
Alejandro Lozano <alex93lzn@gmail.com>
2020-05-26 16:38:00 +0200

    me llevo el metodo de create(formRegistro) y de save al HomeController

--- src/main/java/com/alexlzn/controller/UsuarioController.java
+++ src/main/java/com/alexlzn/controller/UsuarioController.java
@@ -3,8 +3,13 @@ package com.alexlzn.controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,34 +32,19 @@ public class UsuarioController {
 	@Autowired
 	IUsuarioService usuarioService;
 	
+	
+	
 	@GetMapping("/index")
 	public String index(Model model, Usuario usuario) {
 		//LISTA EN UNA TABLE TODAS LAS VACANTES DE LA LISTA
 		model.addAttribute("list_usuarios", usuarioService.findAllUsuarios());
 		return "usuarios/listUsuarios";
 	}
-	@GetMapping("/create")
-	public String registroUsuario(Usuario usuario,Model model) { //th:object="${usuario}" DATA BINDING form con Beans
-		//FORMULARIO PARA CREAR UN USUARIO NUEVO
-		return "usuarios/formRegistro";
-	}
-	@PostMapping("/save")
-	public String saveUsuario(Usuario usuario,BindingResult result,RedirectAttributes attributes,Model model) { //DATA BINDING
-		
-		usuario.setFecha_alta(new Date());//FECHA DEL SISTEMA
-		usuario.setEstatus(1);//POR DEFECTO TODOS LOS USUARIOS QUE SE DEN DE ALTA ESTARAN DESBLOQUEADOS
-		//CREAMOS EL PERFIL QUE LE ASIGNAREMOS A UN NUEVO USUARIO
-		Perfil perfil= new Perfil();
-		perfil.setId(3); //EL ID 3 ES EL USUARIO
-		usuario.agregar(perfil);
-		//GUARDAMOS EL USUARIO EN LA BASE DE DATOS
-		usuarioService.guardar(usuario);
-		System.out.println(usuario);
-		attributes.addFlashAttribute("mensaje", "Usuario registrado correctamente"); //atributo flash en listVacantes
-		return "redirect:/usuarios/index"; //redirect al metodo index.
-		}
+	
+	
+	
 	@GetMapping("/delete/{id}")
-	public String delete(@PathVariable("id") int idUser,Model model, RedirectAttributes attribute) {
+	public String delete(@PathVariable("id") Integer idUser,Model model, RedirectAttributes attribute) {
 		System.out.println("Eliminado el usuario con ID :" + idUser);
 		usuarioService.delete(idUser);
 		attribute.addAttribute("mensaje", "El usuario fue eliminado");
